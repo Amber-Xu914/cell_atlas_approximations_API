224f955ff45be758add7c407502724fe4b999ad9
Amber Xu <amberXu@student.unsw.edu.au>
2025-06-23 23:16:57 +1000

    added 4 new tutorials to sphinx gallery, fix the quickstart file name.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,8 +2,9 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+# Configuration file for the Sphinx documentation builder.
 # Early import forfeits the credit warning for atlasapprox
+
 import os
 
 os.environ["ATLASAPPROX_HIDECREDITS"] = "yes"
@@ -24,23 +25,39 @@ extensions = [
     "sphinx_gallery.gen_gallery",
 ]
 sphinx_tabs_disable_tab_closing = True
+show_source = True
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for sphinx-gallery ----------------------------------------------
+
+def file_name_sort_key(example):
+    return os.path.basename(example)
+
 sphinx_gallery_conf = {
-    "filename_pattern": "/.*.py",
-    "examples_dirs": [
-        "../gallery/python",
-    ],
-    "gallery_dirs": [
-        "python/gallery",
-    ],
+    "filename_pattern": r".*\.py",
+    "examples_dirs": ["../gallery/python"],
+    "gallery_dirs": ["python/gallery"],
+    'within_subsection_order': file_name_sort_key,
+    "remove_config_comments": True, # hides config-style comments like thumbnail_path
+    'download_all_examples': False, # hide zip downloading tab
+    'show_memory': False,            # hide memory source
+    'show_signature': False,
+    'show_api_usage': False,
+    'min_reported_time': float('inf'), # hide running time
 }
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = ['_static']
+html_css_files = ['css/custom.css']
+
+extensions += ["sphinx_new_tab_link"]
+
+# optional tweaks
+new_tab_link_show_external_link_icon = False   # add an icon after each link
+new_tab_link_enable_referrer        = True   # keep the HTTP referrer if you need it
+
