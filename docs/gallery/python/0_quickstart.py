f7afbe202c68abdb08617e5d9c1f93747b40015b
Amber Xu <amberXu@student.unsw.edu.au>
2025-06-23 23:22:55 +1000

    added gallery thumbnails and static files for output styling.

--- docs/gallery/python/0_quickstart.py
+++ docs/gallery/python/0_quickstart.py
@@ -2,7 +2,8 @@
 Quickstart
 ==========
 
-This example shows a quick and easy example of how to use atlasapprox to get and plot the expression of some gees in a specific organ of a specific organism.
+This example shows a quick and easy example of how to use atlasapprox to get and plot the expression of some gees in a
+specific organ of a specific organism.
 """
 
 import matplotlib.pyplot as plt
@@ -19,3 +20,5 @@ expression = api.average(organism="h_sapiens", organ="lung", features=["CD4", "C
 fig, ax = plt.subplots(figsize=(7, 4))
 sns.heatmap(expression, ax=ax)
 fig.tight_layout()
+
+# sphinx_gallery_thumbnail_path = '_static/quickstart.png'
